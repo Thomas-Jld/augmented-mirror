3bf856360f6f6cf91c4cbdef9e4846d48bb031b6
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2020-10-08 16:03:41 +0200

    Mapping to screen

--- TestDocker/reflection/client/sketch.js
+++ TestDocker/reflection/client/sketch.js
@@ -2,6 +2,13 @@ let socket;
 let body_pos = {};
 let display = false;
 
+let xoffset = 5;  // millimeters
+let yoffset = 10;
+
+let screenwidth = 392.85; //millimeters
+let screenheight = 698.4;
+
+
 function setup() {
   fullscreen(true);
   createCanvas(windowWidth, windowHeight);
@@ -18,13 +25,13 @@ function setup() {
 }
 
 function draw() {
-  background(200);
+  background(0);
   socket.emit('next', true);
   for(var part in body_pos) {
     if(body_pos[part] != -1){
-      fill(0);
-      let x = width/2 - body_pos[part][0];
-      let y = height/2 + body_pos[part][1];
+      fill(200);
+      let x = width/2 - width*(body_pos[part][0] - xoffset)/screenwidth;
+      let y = height*(body_pos[part][1] - yoffset)/screenheight;
       ellipse(x ,y , 10);
       text(part, x + 20, y + 20);
     }
