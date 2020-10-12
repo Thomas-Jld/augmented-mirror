654e0b22a38852e1a04235062fd5c4bd1c7ab696
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2020-10-12 14:19:20 +0200

    Drawing lines

--- client/sketch.js
+++ client/sketch.js
@@ -8,12 +8,18 @@ let yoffset = 50;
 let screenwidth = 392.85; //millimeters
 let screenheight = 698.4;
 
+let junctions = [[1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12], [12, 13], [1, 0], [0, 14], [14, 16], [0, 15], [15, 17], [2, 16], [5, 17]]
+let kpt_names = ['nose', 'neck', 'r_sho', 'r_elb', 'r_wri', 'l_sho', 'l_elb', 'l_wri', 'r_hip', 'r_knee', 'r_ank', 'l_hip', 'l_knee', 'l_ank', 'r_eye', 'l_eye', 'r_ear', 'l_ear']
+//let clock;
 
 function setup() {
   fullscreen(true);
   createCanvas(windowWidth, windowHeight);
   socket = io.connect('http://0.0.0.0:5000');
 
+  //clock = new p5(Clock);
+
+  //clock.set(200, 200, width - 200,  200, 0);
 
   socket.on('update',
     function(data) {
@@ -36,4 +42,16 @@ function draw() {
       text(part, x + 20, y + 20);
     }
   }
+  drawLine();
+  //clock.show();
+}
+
+function drawLine(){
+  stroke(255);
+  strokeWeight(4);
+  junctions.forEach(pair => {
+    if(body_pos[keypoints[pair[0]]] != -1 && body_pos[keypoints[pair[1]]] != -1){
+      line(body_pos[keypoints[pair[0]]][0], body_pos[keypoints[pair[0]]][1], body_pos[keypoints[pair[1]]][0], body_pos[keypoints[pair[1]]][1]);
+    }
+  });
 }
