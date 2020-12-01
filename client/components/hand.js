e679a97fc8545c81d3d29d940d0809ee834f099f
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2020-12-01 14:10:09 +0100

    Using ratio

--- client/components/hand.js
+++ client/components/hand.js
@@ -63,21 +63,21 @@ let Hands = ( sketch ) => {
     sketch.stroke(0, 255, 0);
     sketch.strokeWeight(4);
     let ratio = hands_joints[hands_joints.length - 1];
-    ratio[4] = ratio[4] + 0.1;
+    let multi = 0.1;
     junctions.forEach(pair => {
       sketch.stroke(pair[2]*360/7, 255, 255)
       for(let i = 0; i < hands_joints.length - 1; i++){
         let dx1 = hands_joints[i][pair[0]][0]*ratio[0] - ratio[2];
         let dy1 = hands_joints[i][pair[0]][1]*ratio[1] - ratio[3];
-        let xi1 = ratio[2] + ratio[4]*dx1;
-        let yi1 = ratio[3] + ratio[4]*dy1;
+        let xi1 = ratio[2] + (ratio[4]+multi)*dx1;
+        let yi1 = ratio[3] + (ratio[4]+multi)*dy1;
         let x1 = sketch.width/2 + width*(xi1 - xoffset)/screenwidth;
         let y1 = height*(yi1 - yoffset)/screenheight;
 
         let dx2 = hands_joints[i][pair[1]][0]*ratio[0] - ratio[2];
         let dy2 = hands_joints[i][pair[1]][1]*ratio[1] - ratio[3];
-        let xi2 = ratio[2] + ratio[4]*dx2;
-        let yi2 = ratio[3] + ratio[4]*dy2;
+        let xi2 = ratio[2] + (ratio[4]+multi)*dx2;
+        let yi2 = ratio[3] + (ratio[4]+multi)*dy2;
         let x2 = sketch.width/2 + sketch.width*(xi2 - xoffset)/screenwidth;
         let y2 = sketch.height*(yi2 - yoffset)/screenheight;
         sketch.line(x1, y1, x2, y2);
