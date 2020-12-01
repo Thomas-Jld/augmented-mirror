c6a86a663fb90a54fe15df4dbc1afe50bcf09db3
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2020-12-01 13:40:50 +0100

    Using ratio

--- client/components/hand.js
+++ client/components/hand.js
@@ -49,7 +49,7 @@ let Hands = ( sketch ) => {
         let dy = hands_joints[i][index][1]*ratio[1] - ratio[3];
         let xi = ratio[2] + ratio[4]*dx;
         let yi = ratio[3] + ratio[4]*dy;
-        let x = sketch.width*(xi - xoffset)/screenwidth;
+        let x = sketch.width/2 + sketch.width*(xi - xoffset)/screenwidth;
         let y = sketch.height*(yi - yoffset)/screenheight;
         sketch.ellipse(x ,y , 5);
         sketch.text(index, x + 20, y + 20);
@@ -69,14 +69,14 @@ let Hands = ( sketch ) => {
         let dy1 = hands_joints[i][pair[0]][1]*ratio[1] - ratio[3];
         let xi1 = ratio[2] + ratio[4]*dx1;
         let yi1 = ratio[3] + ratio[4]*dy1;
-        let x1 = width*(xi1 - xoffset)/screenwidth;
+        let x1 = sketch.width/2 + width*(xi1 - xoffset)/screenwidth;
         let y1 = height*(yi1 - yoffset)/screenheight;
 
         let dx2 = hands_joints[i][pair[1]][0]*ratio[0] - ratio[2];
         let dy2 = hands_joints[i][pair[1]][1]*ratio[1] - ratio[3];
         let xi2 = ratio[2] + ratio[4]*dx2;
         let yi2 = ratio[3] + ratio[4]*dy2;
-        let x2 = sketch.width*(xi2 - xoffset)/screenwidth;
+        let x2 = sketch.width/2 + sketch.width*(xi2 - xoffset)/screenwidth;
         let y2 = sketch.height*(yi2 - yoffset)/screenheight;
         sketch.line(x1, y1, x2, y2);
       }
