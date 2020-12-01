8ab14159c76f68d7da950bdc98086b180a77e1ca
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2020-12-01 13:31:19 +0100

    To center

--- reflection/get_hand_gesture.py
+++ reflection/get_hand_gesture.py
@@ -28,8 +28,8 @@ def find_hand_pose(hands, frame):
             hand_landmarks = []
             for landmark in hands.landmark:
                 hand_landmarks.append([
-                    landmark.x*frame.shape[0],
-                    landmark.y*frame.shape[1],
+                    (landmark.x-0.5)*frame.shape[0],
+                    (landmark.y-0.5)*frame.shape[1],
                     landmark.presence,
                     landmark.visibility,
                     ])
