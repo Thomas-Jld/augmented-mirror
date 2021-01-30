4c102a3740c050237d914ac99d4a3e1ac61db2a6
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2021-01-30 16:08:47 +0100

    New names and data

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const io = require('socket.io')(5000);
 
-let joint = {};
-let mesh = {};
-let hands = [];
+let body_pose = [];
+let face_mesh = [];
+let right_hand_pose = [];
+let left_hand_pose = [];
 
 io.on('connect', socket => {
   // either with send()
@@ -11,27 +12,20 @@ io.on('connect', socket => {
   // or with emit() and custom event names
   socket.emit('greetings', "Hi !");
 
-  socket.on('joint', (data) => {
-    joint = data;
+  socket.on('global_data', (data) => {
+    body_pose = data["body_pose"];
+    right_hand_pose = data["right_hand_pose"];
+    left_hand_pose = data["left_hand_pose"];
+    face_mesh = data["face_mesh"];
     console.log(data);
   });
 
-
-  socket.on('mesh', (data) => {
-    mesh = data;
-    console.log(data);
-  });
-
-  socket.on('hands', (data) => {
-    hands = data;
-    console.log(data);
-  });
 
   socket.on('nextJoint', (data) => {
-    socket.emit('updateJoint', joint);
+    socket.emit('updateJoint', body_pose);
   });
 
   socket.on('nextHands', (data) => {
-    socket.emit('updateHands', hands);
+    socket.emit('updateHands', right_hand_pose + left_hand_pose);
   });
 });
