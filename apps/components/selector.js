ded1f7bf7d317f53435b3cd721a0d08390d0b479
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2021-03-03 14:51:26 +0100

    Correcting mistakes

--- apps/components/selector.js
+++ apps/components/selector.js
@@ -13,68 +13,73 @@ let Selector = (sketch) => {
         sketch.y = p2;
         sketch.selfCanvas = sketch.createCanvas(sketch.width, sketch.height).position(sketch.x, sketch.y);
         sketch.angleMode(DEGREES);
-        sketch.menu = new Menu(sketch.x, sketch.y, 3, 20, [])
+        sketch.menu = new Menu(sketch.x, sketch.y, 3, 100, [])
     };
 
 
     sketch.show = () => {
         sketch.clear();
         sketch.push();
-        sketch.fill(255, 0, 0);
-        sketch.ellipse(sketch.x, sketch.y, 20);
         for (let i = 0; i < sketch.menu.bubbles.length; i++) {
             sketch.menu.bubbles[i].show();
-            sketch.menu.bubbles[i].update();
+            sketch.menu.bubbles[i].update(sketch.x, sketch.y);
         }
-        sketch.pop();;
+        sketch.pop();
     };
 
 
-    class Menu{
-        constructor(x, y, nb, r, choices){
+    class Menu {
+        constructor(x, y, nb, d, choices) {
             this.x = x;
             this.y = y;
             this.nb = nb;
-            this.r = r;
+            this.d = d;
             this.choices = choices;
-
-            this.slots = [2*Math.PI/3, Math.PI/3, Math.PI, 0];
+    
+            this.slots = [Math.PI / 3, 2 * Math.PI / 3, Math.PI, 0];
             this.bubbles = [];
-
-            for(let i = 0; i <= nb; i++){
-                this.bubbles.push(new Bubble(this.x, this.y, this.slots[i], 20, this.choices[i]));
+    
+            for (let i = 0; i < nb; i++) {
+                this.bubbles.push(new Bubble(this.x, this.y, this.slots[i], this.d, this.choices[i]));
             }
         }
     }
 
     class Bubble {
-        constructor(x, y, angle, r, choice) {
-            this.x = x;
-            this.y = y;
+        constructor(x, y, angle, d, choice) {
+            this.x = x + this.per * this.d * Math.cos(this.angle);
+            this.y = y - this.per * this.d * Math.sin(this.angle);
             this.angle = angle;
-            this.r = r;
+            this.d = d;
             this.choice = choice;
-            this.d = 50;
+            this.r = this.d / 2;
             this.per = 0;
+            this.c = 0;
             this.mul = 0.99;
-            this.color = [100, 0.8];
+            this.color = 200;
         }
-
+    
         show() {
             sketch.stroke(255);
-            sketch.fill(this.color);
-            sketch.ellipse(this.x*(1+this.per*this.d*Math.cos(this.angle)), this.y*(1+this.per*this.d*Math.sin(this.angle)), this.r*this.per);
+            sketch.fill(200, 0.7);
+            sketch.ellipse(this.x , this.y, this.r * this.per);
+            // ellipse(this.x, this.y, this.r * this.per);
         }
-
-        update() {
-            if (!sketch.display_bubbles){
-                this.per = this.per * this.mul;
-            }
-            else{
-                if(this.per != 1){
-                    this.per += 0.02;
+    
+        update(x, y) {
+            this.x = x + this.per * this.d * Math.cos(this.angle);
+            this.y = y - this.per * this.d * Math.sin(this.angle);
+            if (!display_bubbles) {
+                this.per *= this.mul;
+            } else {
+                if (this.per < 1) {
+                    this.per += 0.04;
                 }
             }
         }
+
+        // clicked() {
+            
+        // }
     }
 }
