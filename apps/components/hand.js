50130c49507ef5370192add534215e893b2df923
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2021-03-03 14:53:13 +0100

    Using class instead of functions and adding smoke particules

--- apps/components/hand.js
+++ apps/components/hand.js
@@ -1,179 +1,201 @@
 let Hands = (sketch) => {
-  let hands_joints = [];
-  let display = false;
 
-  let xoffset = 0; // millimeters
-  let yoffset = 100;
+    let xoffset = 0; // millimeters
+    let yoffset = 100;
 
-  let screenwidth = 392.85; //millimeters
-  let screenheight = 698.4;
+    let screenwidth = 392.85; //millimeters
+    let screenheight = 698.4;
 
-  let junctions = [
-    [
-      [0, 1],
-      [0, 5],
-      [0, 9],
-      [0, 13],
-      [0, 17],
-      [5, 9],
-      [9, 13],
-      [13, 17]
-    ],
-    [
-      [1, 2],
-      [2, 3],
-      [3, 4]
-    ],
-    [
-      [5, 6],
-      [6, 7],
-      [7, 8]
-    ],
-    [
-      [9, 10],
-      [10, 11],
-      [11, 12]
-    ],
-    [
-      [13, 14],
-      [14, 15],
-      [15, 16]
-    ],
-    [
-      [17, 18],
-      [18, 19],
-      [19, 20]
-    ]
-  ];
+    let junctions = [
+        [
+            [0, 1],
+            [0, 5],
+            [0, 9],
+            [0, 13],
+            [0, 17],
+            [5, 9],
+            [9, 13],
+            [13, 17]
+        ],
+        [
+            [1, 2],
+            [2, 3],
+            [3, 4]
+        ],
+        [
+            [5, 6],
+            [6, 7],
+            [7, 8]
+        ],
+        [
+            [9, 10],
+            [10, 11],
+            [11, 12]
+        ],
+        [
+            [13, 14],
+            [14, 15],
+            [15, 16]
+        ],
+        [
+            [17, 18],
+            [18, 19],
+            [19, 20]
+        ]
+    ];
 
-  let keypoints = [0, 1, 2, 3, 4, 5,
-    6, 7, 8, 9, 10, 11,
-    12, 13, 14, 15, 16, 17,
-    18, 19, 20
-  ];
+    let keypoints = [0, 1, 2, 3, 4, 5,
+        6, 7, 8, 9, 10, 11,
+        12, 13, 14, 15, 16, 17,
+        18, 19, 20
+    ];
 
-  let xmul = 0.16
-  let ymul = -0.15
+    // let xmul = 0.16
+    // let ymul = -0.15
 
-  let particules = [];
+    let particules = [];
 
-  sketch.movable = false;
-  sketch.latched = false;
-  sketch.activated = true;
-  sketch.clickable = false;
+    sketch.movable = false;
+    sketch.latched = false;
+    sketch.activated = true;
+    sketch.clickable = false;
 
-  sketch.show_particules = true;
-  sketch.show_hands_points = true;
-  sketch.show_hands_lines = true;
+    sketch.show_particules = true;
+    sketch.show_hands_points = true;
+    sketch.show_hands_lines = true;
 
-  sketch.set = (p1, p2, w, h) => {
-    sketch.width = w;
-    sketch.height = h;
-    sketch.x = p1;
-    sketch.y = p2;
-    sketch.selfCanvas = sketch.createCanvas(sketch.width, sketch.height).position(sketch.x, sketch.y);
+    sketch.set = (p1, p2, w, h) => {
+        sketch.width = w;
+        sketch.height = h;
+        sketch.x = p1;
+        sketch.y = p2;
+        sketch.selfCanvas = sketch.createCanvas(sketch.width, sketch.height).position(sketch.x, sketch.y);
 
-    sketch.colorMode(HSB);
-    socket.on('updateHands',
-      function (data) {
-        hands_joints = data;
-      }
-    );
-  };
+        sketch.right_hand = new Hand("right_hand");
+        sketch.left_hand = new Hand("left_hand");
+
+        sketch.colorMode(HSB);
+    };
 
 
-  sketch.show = () => {
-    sketch.selfCanvas.clear();
-    socket.emit('nextHands', true);
-    sketch.show_hands();
-    if (sketch.show_particules) {
-      particules.forEach(particule => {
-        particule.show();
-        particule.update();
-      });
-    }
-  }
+    sketch.show = () => {
+        sketch.selfCanvas.clear();
 
-
-  // sketch.checkSign = () => {
-  //   for (var part in hands_joints) {
-  //     if (part.slice(2, 4) != [-1, -1]) {
-  //       sketch.fill(200);
-  //       let x = width / 2 - width * (part[2] - xoffset) / screenwidth;
-  //       let y = height * (part[3] - yoffset) / screenheight;
-  //       sketch.ellipse(x, y, 30);
-  //       sketch.text(part, x + 20, y + 20);
-  //     }
-  //   }
-  // }
-
-  sketch.show_hands = () => {
-    for (var part in hands_joints) {
-      if (part.slice(2, 4) != [-1, -1]) {
-        sketch.fill(200);
-        let x = width / 2 - width * (part[2] - xoffset) / screenwidth;
-        let y = height * (part[3] - yoffset) / screenheight;
+        sketch.right_hand.show();
+        sketch.left_hand.show();
 
         if (sketch.show_particules) {
-          if (frameCount % 5 == 0) {
-            particules.push(new Particule(x, y));
-          }
+            particules.forEach(particule => {
+                particule.show();
+                particule.update();
+            });
+        }
+    }
+
+    class Hand {
+        constructor(name) {
+            this.junctions = [
+                [
+                    [0, 1],
+                    [0, 5],
+                    [0, 9],
+                    [0, 13],
+                    [0, 17],
+                    [5, 9],
+                    [9, 13],
+                    [13, 17]
+                ],
+                [
+                    [1, 2],
+                    [2, 3],
+                    [3, 4]
+                ],
+                [
+                    [5, 6],
+                    [6, 7],
+                    [7, 8]
+                ],
+                [
+                    [9, 10],
+                    [10, 11],
+                    [11, 12]
+                ],
+                [
+                    [13, 14],
+                    [14, 15],
+                    [15, 16]
+                ],
+                [
+                    [17, 18],
+                    [18, 19],
+                    [19, 20]
+                ]
+            ];
+
+            this.keypoints = [0, 1, 2, 3, 4, 5,
+                6, 7, 8, 9, 10, 11,
+                12, 13, 14, 15, 16, 17,
+                18, 19, 20
+            ];
+
+            this.hand_pose = [];
+            this.name = name;
+
+            setInterval(socket.emit('get_' + this.name, true), 40);
+
+            socket.on('send_' + this.name,
+                this.update(data)
+            );
+        }
+ 
+        show() {
+            if (this.hand_pose == []) {
+                return
+            }
+            let transposed = [];
+            for (var part in this.hand_pose) {
+                sketch.fill(200);
+                let x = width / 2 - width * (part[2] - xoffset) / screenwidth;
+                let y = height * (part[3] - yoffset) / screenheight;
+
+                transposed.push([x, y]);
+
+                if (sketch.show_particules) {
+                    if (frameCount % 5 == 0) {
+                        particules.push(new Particule(x, y));
+                    }
+                }
+
+                if (sketch.show_hands_points) {
+                    sketch.ellipse(x, y, 30);
+                    sketch.text(part, x + 20, y + 20);
+                }
+
+            }
+
+            if (sketch.show_hands_lines) {
+                this.show_lines(transposed);
+            }
         }
 
-        if (sketch.show_hands_points) {
-          sketch.ellipse(x, y, 30);
-          sketch.text(part, x + 20, y + 20);
+        show_lines(transposed) {
+            sketch.stroke(255);
+            sketch.strokeWeight(4);
+            junctions.forEach(parts => {
+                parts.forEach(pair => {
+                    try {
+                        if (transposed[pair[0]][1] > 0 && transposed[pair[1]][1] > 0) {
+                            sketch.line(transposed[pair[0]][0], transposed[pair[0]][1], transposed[pair[1]][0], transposed[pair[1]][1]);
+                        }
+                    } catch (e) {
+                        console.log(e);
+                    }
+                })
+            });
         }
-      }
-    }
-    if (sketch.show_hands_lines) {
-      sketch.draw_hands_lines();
-    }
-  }
 
-  sketch.draw_hands_lines = () => {
-    sketch.stroke(0, 255, 0);
-    sketch.strokeWeight(4);
-    let ratio = hands_joints[hands_joints.length - 1];
-    junctions.forEach(parts => {
-      parts.forEach(pair => {
-        try {
-          if (hands_joints[pair[0]].slice(2, 4) != [-1, -1] && hands_joints[pair[1]].slice(2, 4) != [-1, -1]) {
-            let x1 = width / 2 - width * (hands_joints[pair[0]][2] - xoffset) / screenwidth;
-            let y1 = height * (hands_joints[pair[0]][3] - yoffset) / screenheight;
-            let x2 = width / 2 - width * (hands_joints[pair[1]][2] - xoffset) / screenwidth;
-            let y2 = height * (hands_joints[pair[1]][3] - yoffset) / screenheight;
-            sketch.line(x1, y1, x2, y2);
-          }
-        } catch (e) {}
-      })
-    });
-  }
-
-  class Particule {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.lifespan = 1;
-      this.size = 10 * Math.random();
-      this.backspeed = Math.random();
-      this.latspeed = (Math.random() - 0.5) / 10;
+        update(data) {
+            this.hand_pose = data;
+        }
     }
-
-    show() {
-      stroke(255 * this.lifespan);
-      strokeWeight(this.size);
-      point(this.x, this.y);
-    }
-
-    update() {
-      this.y -= this.backspeed;
-      this.x += this.latspeed;
-      this.latspeed += (Math.random() - 0.5) / 10;
-      this.lifespan *= 0.97;
-      if (this.lifespan < 0.1) {
-        particules = particules.slice(1);
-      }
-    }
-  }
 }
--- apps/components/pose.js
+++ apps/components/pose.js
@@ -1,88 +1,206 @@
-let Pose = ( sketch ) => {
-  let body_pos = {};
-  let display = false;
+let Pose = (sketch) => {
 
-  let xoffset = 20;  // millimeters
-  let yoffset = 50;
+    let xoffset = 20; // millimeters
+    let yoffset = 50;
 
-  let screenwidth = 392.85; //millimeters
-  let screenheight = 698.4;
+    let screenwidth = 392.85; //millimeters
+    let screenheight = 698.4;
 
-  let junctions = [[[ 0, 1],[ 0, 4],[ 1, 2],[ 2, 3],[ 3, 7],
-                    [ 4, 5],[ 5, 6],[ 6, 8]],
-                    [[9,10]],
-                    [[11,12],[11,13],[11,23],[12,14],[12,24],
-                    [13,15],[14,16],[15,17],[15,19],[15,21],
-                    [16,18],[16,20],[16,22],[17,19],[18,20],
-                    [23,24],[23,25],[24,26],[25,27],[26,28],
-                    [27,29],[27,31],[28,30],[28,32]]];
+    let junctions = [
+        [
+            [0, 1],
+            [0, 4],
+            [1, 2],
+            [2, 3],
+            [3, 7],
+            [4, 5],
+            [5, 6],
+            [6, 8]
+        ],
+        [
+            [9, 10]
+        ],
+        [
+            [11, 12],
+            [11, 13],
+            [11, 23],
+            [12, 14],
+            [12, 24],
+            [13, 15],
+            [14, 16],
+            [15, 17],
+            [15, 19],
+            [15, 21],
+            [16, 18],
+            [16, 20],
+            [16, 22],
+            [17, 19],
+            [18, 20],
+            [23, 24],
+            [23, 25],
+            [24, 26],
+            [25, 27],
+            [26, 28],
+            [27, 29],
+            [27, 31],
+            [28, 30],
+            [28, 32]
+        ]
+    ];
 
-  let keypoints = ['nose', 'left_eye_inner', 'left_eye', 'left_eye_outer', 
-                  'right_eye_inner', 'right_eye', 'right_eye_outer',
-                  'left_ear', 'right_ear', 'mouth_left', 'mouth_right', 
-                  'left_shoulder', 'right_shoulder', 'left_elbow',
-                  'right_elbow', 'left_wrist', 'right_wrist', 'left_pinky',
-                  'right_pinky', 'left_index', 'right_index', 'left_thumb',
-                  'right_thumb', 'left_hip', 'right_hip', 'left_knee',
-                  'left_ankle', 'right_ankle', 'left_heel', 'right_heel',
-                  'left_foot_index', 'right_foot_index'];
+    let keypoints = ['nose', 'left_eye_inner', 'left_eye', 'left_eye_outer',
+        'right_eye_inner', 'right_eye', 'right_eye_outer',
+        'left_ear', 'right_ear', 'mouth_left', 'mouth_right',
+        'left_shoulder', 'right_shoulder', 'left_elbow',
+        'right_elbow', 'left_wrist', 'right_wrist', 'left_pinky',
+        'right_pinky', 'left_index', 'right_index', 'left_thumb',
+        'right_thumb', 'left_hip', 'right_hip', 'left_knee',
+        'left_ankle', 'right_ankle', 'left_heel', 'right_heel',
+        'left_foot_index', 'right_foot_index'
+    ];
+
+    let particules;
+
+    sketch.movable = false;
+    sketch.latched = false;
+    sketch.activated = true;
+    sketch.clickable = false;
+
+    sketch.show_particules = true;
+    sketch.show_body_points = true;
+    sketch.show_body_lines = true;
+
+    sketch.set = (p1, p2, w, h) => {
+        sketch.width = w;
+        sketch.height = h;
+        sketch.x = p1;
+        sketch.y = p2;
+        sketch.selfCanvas = sketch.createCanvas(sketch.width, sketch.height).position(sketch.x, sketch.y);
+
+        sketch.pose = new Body("pose");
+    };
 
 
-  sketch.movable = false;
-  sketch.latched = false;
-  sketch.activated = true;
-  sketch.clickable = false;
-
-  sketch.set = (p1, p2, w, h) => {
-    sketch.width = w;
-    sketch.height = h;
-    sketch.x = p1;
-    sketch.y = p2;
-    sketch.selfCanvas = sketch.createCanvas(sketch.width, sketch.height).position(sketch.x, sketch.y);
-
-
-    socket.on('updateJoint',
-      function(data) {
-        body_pos = data;
-        // console.log(data);
-      }
-    );
-  };
-
-
-  sketch.show = () => {
-    sketch.selfCanvas.clear();
-    socket.emit('nextJoint', true);
-    for(var part in body_pos) {
-      if(part.slice(2,4) != [-1,-1]){
-        sketch.fill(200);
-        let x = width/2 - width*(part[2] - xoffset)/screenwidth;
-        let y = height*(part[3] - yoffset)/screenheight;
-        sketch.ellipse(x ,y ,30);
-        sketch.text(part[1], x + 20, y + 20);
-      }
+    sketch.show = () => {
+        sketch.selfCanvas.clear();
+        sketch.pose.show();
     }
-    sketch.drawLine();
-  }
 
-  sketch.drawLine = () => {
-    sketch.stroke(255);
-    sketch.strokeWeight(4);
-    junctions.forEach(parts => {
-      parts.forEach(pair => {
-        try {
-          if (body_pos[pair[0]].slice(2, 4) != [-1, -1] &&
-            body_pos[pair[1]].slice(2, 4) != [-1, -1]) {
-            let x1 = width / 2 - width * (body_pos[pair[0]][2] - xoffset) / screenwidth;
-            let y1 = height * (body_pos[pair[0]][3] - yoffset) / screenheight;
-            let x2 = width / 2 - width * (body_pos[pair[1]][2] - xoffset) / screenwidth;
-            let y2 = height * (body_pos[pair[1]][3] - yoffset) / screenheight;
-            if (y1 > 0 && y2 > 0) {
-              sketch.line(x1, y1, x2, y2);
+
+    class Body {
+        constructor(name) {
+            let junctions = [
+                [
+                    [0, 1],
+                    [0, 4],
+                    [1, 2],
+                    [2, 3],
+                    [3, 7],
+                    [4, 5],
+                    [5, 6],
+                    [6, 8]
+                ],
+                [
+                    [9, 10]
+                ],
+                [
+                    [11, 12],
+                    [11, 13],
+                    [11, 23],
+                    [12, 14],
+                    [12, 24],
+                    [13, 15],
+                    [14, 16],
+                    [15, 17],
+                    [15, 19],
+                    [15, 21],
+                    [16, 18],
+                    [16, 20],
+                    [16, 22],
+                    [17, 19],
+                    [18, 20],
+                    [23, 24],
+                    [23, 25],
+                    [24, 26],
+                    [25, 27],
+                    [26, 28],
+                    [27, 29],
+                    [27, 31],
+                    [28, 30],
+                    [28, 32]
+                ]
+            ];
+
+            let keypoints = ['nose', 'left_eye_inner', 'left_eye', 'left_eye_outer',
+                'right_eye_inner', 'right_eye', 'right_eye_outer',
+                'left_ear', 'right_ear', 'mouth_left', 'mouth_right',
+                'left_shoulder', 'right_shoulder', 'left_elbow',
+                'right_elbow', 'left_wrist', 'right_wrist', 'left_pinky',
+                'right_pinky', 'left_index', 'right_index', 'left_thumb',
+                'right_thumb', 'left_hip', 'right_hip', 'left_knee',
+                'left_ankle', 'right_ankle', 'left_heel', 'right_heel',
+                'left_foot_index', 'right_foot_index'
+            ];
+
+
+            this.body_pose = [];
+            this.name = name;
+
+            setInterval(socket.emit('get_' + this.name, true), 40);
+
+            socket.on('send_' + this.name,
+                this.update(data)
+            );
+        }
+
+        show() {
+            if (this.body_pose == []) {
+                return
             }
-          }
-        } catch (e) {}
-      })
-    });
-  }
+            let transposed = [];
+            for (var part in this.body_pose) {
+                sketch.fill(200);
+                let x = width / 2 - width * (part[2] - xoffset) / screenwidth;
+                let y = height * (part[3] - yoffset) / screenheight;
+
+                transposed.push([x, y]);
+
+                if (sketch.show_particules) {
+                    if (frameCount % 5 == 0) {
+                        particules.push(new Particule(x, y));
+                    }
+                }
+
+                if (sketch.show_body_points) {
+                    sketch.ellipse(x, y, 30);
+                    sketch.text(part[1], x + 20, y + 20);
+                }
+
+            }
+
+            if (sketch.show_body_lines) {
+                this.show_lines(transposed);
+            }
+        }
+
+        show_lines(transposed) {
+            sketch.stroke(255);
+            sketch.strokeWeight(4);
+            junctions.forEach(parts => {
+                parts.forEach(pair => {
+                    try {
+                        if (transposed[pair[0]][1] > 0 && transposed[pair[1]][1] > 0) {
+                            sketch.line(transposed[pair[0]][0], transposed[pair[0]][1], transposed[pair[1]][0], transposed[pair[1]][1]);
+                        }
+                    } catch (e) {
+                        console.log(e);
+                    }
+                })
+            });
+        }
+
+        update(data) {
+            this.body_pose = data;
+        }
+    }
 }
