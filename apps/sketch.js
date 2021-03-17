dc488b9a59c49c2ae09730a3b37faff848cdd47b
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2021-03-17 11:08:23 +0100

    Displaying menu or not

--- apps/sketch.js
+++ apps/sketch.js
@@ -1,4 +1,3 @@
-
 let modules = [];
 let objectsSelected = false;
 
@@ -19,145 +18,150 @@ let screenwidth = 392.85; //millimeters
 let screenheight = 698.4;
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+    canvas = createCanvas(windowWidth, windowHeight);
 
-  socket = io.connect('http://0.0.0.0:5000');
+    socket = io.connect('http://0.0.0.0:5000');
 
-  frameRate(30);
+    frameRate(30);
 
-  setTimeout(reshape, 1000);
+    setTimeout(reshape, 1000);
 }
 
 function draw() {
-  background(0);
-  if(started){
-    modules.forEach(m => {
-      if(m.activated){
-        m.show();
-        if(m.latched){
-          m.setPosition(mouseX - m.OffsetX, mouseY - m.OffsetY);
+    background(0);
+    if (started) {
+        modules.forEach(m => {
+            if (m.activated) {
+                m.show();
+                if (m.latched) {
+                    m.setPosition(mouseX - m.OffsetX, mouseY - m.OffsetY);
+                }
+            }
+        });
+        if (hands.left_hand.hand_pose_t[8] !== undefined && hands.left_hand.hand_pose_t[5] !== undefined) {
+            if (hands.left_hand.hand_pose_t[5][1] - hands.left_hand.hand_pose_t[8][1] > 5){
+                selector.display_bubbles = true;
+            }
+            else{
+                selector.display_bubbles = false;
+            }
+            selector.mx = hands.left_hand.hand_pose_t[8][0];
+            selector.my = hands.left_hand.hand_pose_t[8][1];
         }
-      }
-    });
-    if(hands.left_hand.hand_pose_t[8] !== undefined){
-      selector.mx = hands.left_hand.hand_pose_t[8][0];
-      selector.my = hands.left_hand.hand_pose_t[8][1];
     }
-  }
 }
 
-function reshape(){
-  resizeCanvas(windowWidth, windowHeight);
+function reshape() {
+    resizeCanvas(windowWidth, windowHeight);
 
-  // let clock = new p5(Clock);
-  // clock.set(width - 200, 0, 200, 200);
-  // modules.push(clock);
+    // let clock = new p5(Clock);
+    // clock.set(width - 200, 0, 200, 200);
+    // modules.push(clock);
 
-  // let bubble = new p5(HeartBubble);
-  // bubble.set(0, 0, width, height);
-  // modules.push(bubble);
+    // let bubble = new p5(HeartBubble);
+    // bubble.set(0, 0, width, height);
+    // modules.push(bubble);
 
-  // let multiplications = new p5(Multiplications);
-  // multiplications.set(0, 0, 150, 150);
-  // modules.push(multiplications);
+    // let multiplications = new p5(Multiplications);
+    // multiplications.set(0, 0, 150, 150);
+    // modules.push(multiplications);
 
-  pose = new p5(Pose);
-  pose.set(0, 0, width, height);
-  modules.push(pose);
+    pose = new p5(Pose);
+    pose.set(0, 0, width, height);
+    modules.push(pose);
 
-  hands = new p5(Hands);
-  hands.set(0, 0, width, height);
-  modules.push(hands);
+    hands = new p5(Hands);
+    hands.set(0, 0, width, height);
+    modules.push(hands);
 
-  face = new p5(Faces);
-  face.set(0, 0, width, height);
-  modules.push(face);
+    face = new p5(Faces);
+    face.set(0, 0, width, height);
+    modules.push(face);
 
-  selector = new p5(Selector);
-  selector.set(0,0, width, height);
-  modules.push(selector);
+    selector = new p5(Selector);
+    selector.set(0, 0, width, height);
+    modules.push(selector);
 
-  started = true;
+    started = true;
 }
 
-function keyPressed(){
-  if(key == "c"){
-    modules.forEach(m => {
-      m.clearSketch();
-    });
-  }
-
-  if(key == "p"){
-    modules.forEach(m => {
-      m.activated = false;
-      m.selfCanvas.hide();
-    });
-    pose.activated = true;
-    pose.selfCanvas.show();
-  }
-
-  if(key == "h"){
-    modules.forEach(m => {
-      m.activated = false;
-      m.selfCanvas.hide();
-    });
-    hands.activated = true;
-    hands.selfCanvas.show();
-  }
-
-  if(key == "f"){
-    modules.forEach(m => {
-      m.activated = false;
-      m.selfCanvas.hide();
-    });
-    face.activated = true;
-    face.selfCanvas.show();
-  }
-
-  if(key == "b"){
-    modules.forEach(m => {
-      m.activated = false;
-      m.selfCanvas.hide();
-    });
-    pose.activated = true;
-    pose.selfCanvas.show();
-    hands.activated = true;
-    hands.selfCanvas.show();
-  }
-
-  if(key == "a"){
-    modules.forEach(m => {
-      m.activated = true;
-      m.selfCanvas.show();
-    });
-  }
-}
-
-function mousePressed(){
-  let selected = false;
-  let objectSelected = false;
-  modules.forEach(m => {
-    if(m.movable && mouseX > m.x && mouseY > m.y && mouseX < m.x + m.width && mouseY < m.y + m.height){
-      selected = true;
-      if(!objectsSelected){
-        m.latched = true;
-        objectSelected = true;
-        m.OffsetX = mouseX - m.x;
-        m.OffsetY = mouseY - m.y;
-      }
-      else {
-        m.latched = false;
-      }
+function keyPressed() {
+    if (key == "c") {
+        modules.forEach(m => {
+            m.clearSketch();
+        });
     }
-  });
 
-  objectsSelected = objectSelected;
+    if (key == "p") {
+        modules.forEach(m => {
+            m.activated = false;
+            m.selfCanvas.hide();
+        });
+        pose.activated = true;
+        pose.selfCanvas.show();
+    }
 
-  if(!selected){
+    if (key == "h") {
+        modules.forEach(m => {
+            m.activated = false;
+            m.selfCanvas.hide();
+        });
+        hands.activated = true;
+        hands.selfCanvas.show();
+    }
+
+    if (key == "f") {
+        modules.forEach(m => {
+            m.activated = false;
+            m.selfCanvas.hide();
+        });
+        face.activated = true;
+        face.selfCanvas.show();
+    }
+
+    if (key == "b") {
+        modules.forEach(m => {
+            m.activated = false;
+            m.selfCanvas.hide();
+        });
+        pose.activated = true;
+        pose.selfCanvas.show();
+        hands.activated = true;
+        hands.selfCanvas.show();
+    }
+
+    if (key == "a") {
+        modules.forEach(m => {
+            m.activated = true;
+            m.selfCanvas.show();
+        });
+    }
+}
+
+function mousePressed() {
+    let selected = false;
+    let objectSelected = false;
     modules.forEach(m => {
-      if(!m.movable && m.clickable){
-        m.onClicked(mouseX, mouseY);
-      }
+        if (m.movable && mouseX > m.x && mouseY > m.y && mouseX < m.x + m.width && mouseY < m.y + m.height) {
+            selected = true;
+            if (!objectsSelected) {
+                m.latched = true;
+                objectSelected = true;
+                m.OffsetX = mouseX - m.x;
+                m.OffsetY = mouseY - m.y;
+            } else {
+                m.latched = false;
+            }
+        }
     });
-  }
+
+    objectsSelected = objectSelected;
+
+    if (!selected) {
+        modules.forEach(m => {
+            if (!m.movable && m.clickable) {
+                m.onClicked(mouseX, mouseY);
+            }
+        });
+    }
 }
