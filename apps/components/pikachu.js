b7a467f5219d4eaa80b4317a3e510ce16d8c8d71
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2021-08-22 04:11:08 +0200

    Scale and errors

--- apps/components/pikachu.js
+++ apps/components/pikachu.js
@@ -6,9 +6,10 @@ let Pikachu = (sketch) => {
     sketch.latched = false;
     sketch.activated = false;
     sketch.clickable = true;
+    sketch.to_update = true;
 
     sketch.pikachu_model;
-    sketch.cursor = [100, 100, 1];
+    sketch.cursor = [100, 100, 100];
 
     sketch.set = (p1, p2, w, h) => {
         sketch.width = w;
@@ -22,13 +23,17 @@ let Pikachu = (sketch) => {
         sketch.activated = true;
     };
 
+    sketch.update = (data) => {
+        if(data["right_hand_sign"] != undefined){
+            sketch.pikachu_model.sign = data["right_hand_sign"]
+        }
+    }
+
 
     sketch.show = () => {
         sketch.clear();
-        sketch.push();
         sketch.pikachu_model.show();
         sketch.pikachu_model.update(sketch.cursor[0], sketch.cursor[1], sketch.cursor[2]);
-        sketch.pop();
     };
 
 
@@ -38,14 +43,20 @@ let Pikachu = (sketch) => {
             this.y = y;
             this.s = s;
             this.model = m;
+            this.sign = ["", 0]
         }
         show() {
-            sketch.push();
-            sketch.translate(parseInt(this.x), parseInt(this.y), 0);
-            sketch.scale(this.s);
-            sketch.normalMaterial(); 
-            sketch.model(this.model);
-            sketch.pop();
+            if(this.sign[0] == "INDEX" && this.sign[1] > 0.6){
+                sketch.push();
+                sketch.translate(this.x - width/2, this.y - height/2, 0);
+                sketch.rotateZ(PI);
+                sketch.rotateY(0.01* frameCount);
+                sketch.fill(255);
+                sketch.scale(this.s);
+                sketch.normalMaterial();
+                sketch.model(this.model);
+                sketch.pop();
+            }
         }
         update(x, y, s) {
             this.x = x;
