c5e1ec9bf23639b6230ffb67c659e0b717a7fb95
Thomas <thomas.juldo@edu.devinci.fr>
2021-09-23 18:29:13 +0200

    Cornering warning signs

--- apps/waiting/sketch.js
+++ apps/waiting/sketch.js
@@ -32,25 +32,25 @@ function draw() {
   textAlign(CENTER);
   text('Work in progress\nTravaux en cours\nTrabajo en progreso\nIn Arbeit\n진행중인 작업 ', width/2, height/2+100);
   push();
-  translate(width/2-270,150);
+  translate( 150, 150);
   rotate(angle/8);
   image(img, -75,-75, 150,150);
   pop();
 
   push();
-  translate(width/2+270,150);
+  translate(width-150,150);
   rotate(angle/8);
   image(img, -75,-75, 150,150);
   pop();
 
   push();
-  translate(width/2-270,width/2-350);
+  translate(150,height-150);
   rotate(angle/8);
   image(img, -75,-75, 150,150);
   pop();
 
   push();
-  translate(width/2+270,width/2-350);
+  translate(width-150,height-150);
   rotate(angle/8);
   image(img, -75,-75, 150,150);
   pop();
